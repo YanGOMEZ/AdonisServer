ad33bfd37be5f646f097140d41d24806330f8693
YanGOMEZ <86815266+YanGOMEZ@users.noreply.github.com>
2022-03-30 14:17:28 -0600

    gia

--- database/migrations/1648668868158_barcos.ts
+++ database/migrations/1648668868158_barcos.ts
@@ -6,6 +6,7 @@ export default class Barcos extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.integer('partida').unsigned().references('id').inTable('partidas').notNullable()
       table.integer('jugador').unsigned().references('id').inTable('usuarios').notNullable()
       table.integer('barco').notNullable()
       table.string('posicion',2).notNullable()
