a0a3a3da59ebdd4d38c3ba9bb329cc4d2437897f
YanGOMEZ <86815266+YanGOMEZ@users.noreply.github.com>
2022-03-30 16:10:21 -0600

    puleado

--- app/Controllers/Http/BarcosController.ts
+++ app/Controllers/Http/BarcosController.ts
@@ -44,34 +44,43 @@ export default class BarcosController {
     public async storej1({request, response, auth}:HttpContextContract){
         try{
             for (let index = 0; index < 15; index++) {
+                var arreglo = [0,0,0,0,0,0,0,0]
                 await auth.use('api').authenticate()
                 console.log(auth.use('api').user!)
                 const partida = request.input('partida')
                 const jugador = request.input('jugador')
                 const barco = index + 1
-                var numero = this.numerosAleatoriosNoRepetidos(1,8,8)
-                var po2
-                var tabla = 0
+                var ale = Math.random() * (8-1) + 1
+                var numero = Math.floor(ale)
                 if(index == 0){
-                    numero = this.numerosAleatoriosNoRepetidos(1,8,8)
-                    po2 = 'A'
+                    arreglo[0] = numero
+                }else{
+                    for (let index = 1; index < 15; index++) {
+                        for (let indexc = index; indexc < 8; indexc++) {
+                            if(arreglo[indexc] != numero){
+                                arreglo[indexc] = numero
+                            }
+                            else{
+                                var ale = Math.random() * (8-1) + 1
+                                numero = Math.floor(ale)
+                            }
+                        }
+                    }
                 }
-                else if(index == 4){
-                    numero = this.numerosAleatoriosNoRepetidos(1,8,8)
+                var po2
+                if(index >= 0 && index < 4){
+                    po2 ='A'
+                }
+                else if(index >= 4 && index <8){
                     po2 = 'B'
                 }
-                else if(index == 8){
-                    numero = this.numerosAleatoriosNoRepetidos(1,8,8)
+                else if(index >= 8 && index <12){
                     po2 = 'C'
                 }
-                else if(index == 12){
-                    numero = this.numerosAleatoriosNoRepetidos(1,8,8)
+                else if(index >= 12){
                     po2 = 'D'
                 }
-                if(tabla == 7){
-                    tabla = 0
-                }
-                const posicion = numero[tabla].toString() + po2
+                const posicion = numero.toString() + po2
                 const derribado = 'NO'
                 await Barco.create({partida, jugador, barco, posicion, derribado});
             }
